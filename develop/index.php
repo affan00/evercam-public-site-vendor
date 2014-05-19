61d2e1526e8fa14f50f1803b099550db96f6892d
Ciaran Hanrahan <ciaran@evercam.io>
2014-05-19 11:48:36 +0100

    adding 404 page

--- develop/index.php
+++ develop/index.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-<title>evercam.io/develop</title>
+<title>evercam.io/notfound</title>
 
 <!-- Bootstrap core CSS -->
 <link href="../css/bootstrap.css" rel="stylesheet">
