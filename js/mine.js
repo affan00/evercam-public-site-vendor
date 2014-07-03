d31ddd1fe1045978cb1b81cd49387192b1ccff0b
Ciaran Hanrahan <ciaran@evercam.io>
2014-07-03 12:02:17 +0100

    analytics test1

--- js/mine.js
+++ js/mine.js
@@ -6,7 +6,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-48470473-1', 'auto', {'allowLinker': true});
+  ga('create', 'UA-48470473-1', 'evercam.io', {'allowLinker': true});
   ga('require', 'linker');
   ga('linker:autoLink', ['dashboard.evercam.io', 'api.evercam.io'] );
   ga('send', 'pageview');
