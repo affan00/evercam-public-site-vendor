0110342446e66d0ad1a45339e61d869428ea2660
Ciaran Hanrahan <ciaran@evercam.io>
2015-02-10 16:30:21 +0000

    developer docs swagger link to pure swagger in new tab

--- develop/sidebar.php
+++ develop/sidebar.php
@@ -5,7 +5,7 @@
     API V1
     <ul class="sub-menu">
       <li><a href='/develop/'>Develop</a></li>
-      <li><a href='/develop/docs'>API Docs</a></li>
+      <li><a href='https://dashboard.evercam.io/swagger' target="_blank">API Docs <span class="glyphicon glyphicon-new-window"></span></a></li>
       <li><a href='/develop/authentication'>Authentication</a></li>
       <!--<li><a href='/docs/swagger'>Docs</a></li>-->
       <!--<li><a href='/develop/evercamjs'>Evercam.js</a></li>-->
@@ -20,27 +20,6 @@
     </ul>
   </li>
 </ul>
-<!--<ul>
-  <li class="doc-categories">
-  <a href='/develop/help'>Help Pages</a>
-    <ul class="sub-menu">
-      <li><a href='/develop/help'>Common Issues</a></li>
-      <li><a href='/develop/help'>Using Evercam</a></li>
-      <li><a href='/develop/help'>Camera Setup</a></li>
-      <li><a href='/develop/help'>Accounts</a></li>
-      <li><a href='/develop/help'>Pricing</a></li>
-    </ul>
-  </li>
-</ul>-->
-<!--<ul>
-  <li class="doc-categories">
-  Widgets
-    <ul class="sub-menu">
-      <li><a href='/develop/widget-live'>Live View</a></li>
-      <li><a href='/develop/widget-recorded'>Recorded Images</a></li>
-    </ul>
-  </li>
-</ul>-->
 <ul>
   <li class="doc-categories">
     Connect
@@ -49,9 +28,4 @@
     </ul>
   </li>
 </ul>
-<!--<ul>
-  <li class="doc-categories">
-    <a href="/develop/changelog">Changelog</a>
-  </li>
-</ul>-->
 </div>
