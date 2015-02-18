fe7f9ac5cda764f152713c6345f2b3f325b5f5d2
Ciaran Hanrahan <ciaran@evercam.io>
2015-02-18 11:52:51 +0000

    more dashboard links

--- header.php
+++ header.php
@@ -1,23 +1,20 @@
 <div id="navbar" class="navbar navbar-inverse">
-      <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
+  <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
         <div class="branding"><a class="logo" href="/"><img src="/img/logo.png"></a></div>
-          
-          </div>
-          <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-              <li><a href="/connect">/connect</a></li>
-              <li><a href="/develop/">/develop</a></li>
-              <li><a href="/apps">/apps</a></li>
-              <!--<li><a href="https://dashboard.evercam.io/signup">create account</a></li>-->
-              <li><a class="create-menu" id="sign-in" href="https://dash.evercam.io/signin">sign in</a></li>
-        
-            </ul>
-          </div><!--/.nav-collapse -->
-        </div>
-    </div>    
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="/connect">/connect</a></li>
+          <li><a href="/develop/">/develop</a></li>
+          <li><a href="/apps">/apps</a></li>
+          <li><a class="create-menu" id="sign-in" href="https://dash.evercam.io/signin">sign in</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+</div>    
