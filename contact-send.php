bbaadbf7407246f7d65bae3e0340f369317a6a90
Ciaran Hanrahan <ciaran@evercam.io>
2015-04-16 12:02:03 +0100

    add sendgrid subscribe

--- contact-send.php
+++ contact-send.php
@@ -1,19 +1,65 @@
-<?php
+<footer>      
+  <div class="container">
+    <div class="row">
+      <div class="footer-menu">
+        <ul>
+          <h3>Company</h3>
+          <li><a href="/blog">Blog</a></li>
+          <!--<li><a href="#">About</a></li>-->
+          <!--<li><a href="#">Policy</a></li>-->
+          <li><a href="/team">Team</a></li>
+          <!--<li><a href="#">Jobs</a></li>-->
+          <li><a href="/terms">Terms</a></li>
+          <li><a href="/contact">Contact</a></li>
+          <li><a href="/values">Values</a></li>
+        </ul>
+        <ul>
+          <h3>Developer</h3>
+          <li><a href="/develop">Develop</a></li>
+          <li><a href="https://dash.evercam.io/swagger" target="_blank">Documentation <span class="ti-arrow-top-right"></span></a></li>
+          <!--<li><a href="#">Pricing</a></li>-->
+          <!--<li><a href="/uses">Use Cases</a></li>-->
+          <!--<li><a href="#">Help</a></li>-->
+          
+          <h3>Manufacturer</h3>
+          <li><a href="/camera-manufacturers">Why OEM Evercam? </a></li>
+        </ul>
+        <ul>
+          <h3>Camera Owner</h3>
+          <li><a href="/apps">App Store</a></li>
+          <li><a href="/uses">Use Cases</a></li>
+          <!--<li><a href="#">Help</a></li>-->
+        </ul>
+      </div>
+      <div class="footer-about">
+        <div class="footer-connect">
+            <h3>Connect</h3>
+            <p>Subscribe to our newsletter</p>
+            <div class="sendgrid-subscription-widget" data-token="aC%2F8dfsfHE91irEczhV9ClBlsxsJ3bsi1DJ6HDCJdSYKMN5NOTtQKu6TO7MpLFG8"></div>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://s3.amazonaws.com/subscription-cdn/0.2/widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document, "script", "sendgrid-subscription-widget-js");</script>
+        </div>
+           <!--<div class="input-group">
+              <input type="text" class="form-control" placeholder="Enter your email">
+            </div>-->
+         </div>
+           <ul>            
+            <li><a href="https://github.com/evercam" target="_blank"><i class="fa fa-github-alt"></i> Github</a></li>
+            <li><a href="https://twitter.com/evrcm" target="_blank"><i class="fa fa-twitter"></i> Twitter</a></li>
+            <li><a href="https://www.facebook.com/evrcm" target="_blank"><i class="fa fa-facebook"></i> Facebook</a></li>
+            <li><a href="https://medium.com/evercam-blog" target="_blank"><i class="fa fa-medium"></i> Medium</a></li>
+          </ul>        
+           <ul>
+            <li><a href="https://angel.co/evercam" target="_blank"><i class="fa fa-angellist"></i> AngelList</a></li>
+            <li><a href="https://plus.google.com/+Evercam/videos" target="_blank"><i class="fa fa-google-plus"></i> Google+</a></li>
+            <li><a href="https://www.linkedin.com/company/evercam" target="_blank"><i class="fa fa-linkedin"></i> LinkedIn</a></li>
+          </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" async></script>
+<script src="../js/bootstrap.min.js" async></script>
+<script src="../js/custom.min.js" async></script>
 
-    $to = "ciaran@evercam.io";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $headers = "From: $from";
-    $subject = "You have a message.";
 
-    $fields = array();
-    $fields{"name"} = "name";
-    $fields{"email"} = "email";
-    $fields{"phone"} = "phone";
-    $fields{"message"} = "message";
 
-    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
-
-    $send = mail($to, $subject, $body, $headers);
-
-?>
