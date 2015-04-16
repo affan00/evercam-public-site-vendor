c67c97bc9e59762c059e7e10b5f972a8bffe7b82
Ciaran Hanrahan <ciaran@evercam.io>
2015-04-16 14:27:49 +0100

    contact form and twitter on contact page

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
           <li><a href="/values">Values</a></li>
           <!--<li><a href="#">Jobs</a></li>-->
           <li><a href="/terms">Terms</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li><a href="/contact">Contact</a></li>          
         </ul>
         <ul>
           <h3>Developer</h3>
@@ -26,6 +26,7 @@
           <h3>Camera Owner</h3>
           <li><a href="/apps">App Store</a></li>
           <li><a href="/uses">Use Cases</a></li>
+
         </ul>
       </div>
       <div class="footer-about">
@@ -34,21 +35,24 @@
             <div class="subscribe">
               <p>Subscribe to our newsletter</p>
               <div class="sendgrid-subscription-widget" data-token="aC%2F8dfsfHE91irEczhV9ClBlsxsJ3bsi1DJ6HDCJdSYKMN5NOTtQKu6TO7MpLFG8">
+              <from
               </div>
               <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?"http":"https";if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://s3.amazonaws.com/subscription-cdn/0.2/widget.min.js";fjs.parentNode.insertBefore(js,fjs);}}(document, "script", "sendgrid-subscription-widget-js");</script>
             </div>
          </div>
+         <div class="social-links">
            <ul>            
             <li><a href="https://github.com/evercam" target="_blank"><i class="fa fa-github-alt"></i> Github</a></li>
+            <li><a href="https://medium.com/evercam-blog" target="_blank"><i class="fa fa-medium"></i> Medium</a></li>
             <li><a href="https://twitter.com/evrcm" target="_blank"><i class="fa fa-twitter"></i> Twitter</a></li>
             <li><a href="https://www.facebook.com/evrcm" target="_blank"><i class="fa fa-facebook"></i> Facebook</a></li>
-            <li><a href="https://medium.com/evercam-blog" target="_blank"><i class="fa fa-medium"></i> Medium</a></li>
           </ul>        
-           <ul>
-            <li><a href="https://angel.co/evercam" target="_blank"><i class="fa fa-angellist"></i> AngelList</a></li>
-            <li><a href="https://plus.google.com/+Evercam/videos" target="_blank"><i class="fa fa-google-plus"></i> Google+</a></li>
+           <ul>            
             <li><a href="https://www.linkedin.com/company/evercam" target="_blank"><i class="fa fa-linkedin"></i> LinkedIn</a></li>
+            <li><a href="https://plus.google.com/+Evercam/videos" target="_blank"><i class="fa fa-google-plus"></i> Google+</a></li>            
+            <li><a href="https://angel.co/evercam" target="_blank"><i class="fa fa-angellist"></i> AngelList</a></li>
           </ul>
+        </div>
       </div>
     </div>
   </div>
