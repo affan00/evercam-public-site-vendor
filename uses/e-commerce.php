2cf95a28c9a078891517f4a4053f29b77a8a228c
Ciaran Hanrahan <ciaran@evercam.io>
2015-05-01 17:07:38 +0100

    cloud-cctv use case

--- uses/e-commerce.php
+++ uses/e-commerce.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Evercam -  Cloud CCTV</title>
+  <title>Evercam - e-Commerce</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="keywords" content="cloud, cctv, cameras, use cases, sap, integration, apps, recording, remote storage, sharing, api, developer platform">
-  <meta name="description" content="Cloud CCTV with Evercam. Extend conventional CCTV solutions into the cloud and add remote storage, sharing, access from any mobile device, time-lapses, notifications, logs.">
+  <meta name="keywords" content="e-commerce, cctv, cameras, use cases, integration, apps, recording, remote storage, sharing, api, developer platform">
+  <meta name="description" content="e-Commerce with Evercam. Extend conventional CCTV solutions into the cloud and add remote storage, sharing, access from any mobile device, time-lapses, notifications, logs.">
   <meta name="author" content="Evercam">
   <!-- Bootstrap -->
   <link href="/css/main.css" rel="stylesheet">
@@ -17,8 +17,8 @@
     <header>
       <div class="banner">
         <div class="banner-content">
-          <h1>Cloud CCTV</h1>
-          <h2>Connect any IP camera to Evercam and extend your conventional CCTV solutions into the cloud.</h2>
+          <h1>e-Commerce</h1>
+          <h2></h2>
         </div>
       </div>
     </header>
@@ -28,17 +28,17 @@
     <div class="row">
       <div class="develop-feature">
       <i class="fa fa-cloud-download fa-2x circle"></i>
-        <h3>Cloud Storage</h3>
+        <h3></h3>
         <p>Footage from your camera is recorded and stored in the cloud.</p>
       </div>
       <div class="develop-feature">
       <i class="fa fa-users fa-2x circle"></i>
-        <h3>Sharing</h3>
+        <h3></h3>
         <p>Easily share your camera with concerned parties with appropriate access rights.</p>
       </div>
       <div class="develop-feature">
         <i class="fa fa-mobile fa-2x circle"></i>
-        <h3>Remote Access</h3>
+        <h3></h3>
         <p>Access and view your camera from anywhere, on any device.</p>
       </div>
     </div>
