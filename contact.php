360c6dcd943a59847fdc207586a097dbee36473a
Maciej Goszczycki <mgoszcz2@gmail.com>
2015-07-02 13:59:45 +0100

    Change address & tooltip text

--- contact.php
+++ contact.php
@@ -2,47 +2,49 @@
 
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<title>Evercam -  get in touch with us.</title>
-<meta content="IE=edge" http-equiv="X-UA-Compatible">
-<meta content="width=device-width, initial-scale=1" name="viewport">
-<meta name="keywords" content="cameras, apps, integration, recording, remote storage, sharing, api, developer platform">
-<meta name="description" content="Add remote storage, sharing, time-lapses, notifications, logs, access from any mobile device. ERP Integration - for Construction Site monitoring, Manufacturing, Weighbridge and more.">
-<meta name="author" content="Evercam">
-<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&signed_in=true&libraries=places,geometry"></script>
-<title>Evercam.io</title>
-<link href="css/main.css" rel="stylesheet">  
+  <meta charset="utf-8">
+  <title>Evercam -  get in touch with us.</title>
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <meta name="keywords" content="cameras, apps, integration, recording, remote storage, sharing, api, developer platform">
+  <meta name="description" content="Add remote storage, sharing, time-lapses, notifications, logs, access from any mobile device. ERP Integration - for Construction Site monitoring, Manufacturing, Weighbridge and more.">
+  <meta name="author" content="Evercam">
+  <script src="https://maps.googleapis.com/maps/api/js?v=3"></script>
+  <title>Evercam.io</title>
+  <link href="css/main.css" rel="stylesheet">
 </head>
 <body>
-<? include 'nav.php'; ?>          
-<div id="map"></div>
+  <? include 'nav.php'; ?>
+  <div id="map"></div>
   <section>
-   <div id="contact">
-    <div class="address">
-  <h1>Contact</h1>
-    <p>Wayra Ireland<br />
-    29 Sir John Rogerson's Quay<br />
-    Dublin 2<br />
-    Ireland</p>
-    <p>+353 86 196 0451</p>
-    <p>hello@evercam.io</p>  
-    <p><i class="fa fa-twitter"></i> <a href="https://twitter.com/evrcm" target="_blank">@evrcm</a></p>              
-  </div>
-  <div class="contact-form">
-    <form accept-charset="UTF-8" action="https://formkeep.com/f/3295d2de6a4c" method="POST">
-    <input type="hidden" name="utf8" value="✓">
-    <input type="text" name="name" placeholder="Your Name">
-    <input type="email" name="email" placeholder="Your Email">
-    <textarea type="textarea" rows="4" name="message" placeholder="Your message"></textarea>
-    <button class="btn btn-grey-outline" type="submit">Submit</button>
-    </form>
-  </div>
-  </div>
+    <div id="contact">
+      <div class="address">
+        <h1>Contact</h1>
+        <p>
+          Evercam<br>
+          79 Merrion Square South<br>
+          Dublin 2<br>
+          Ireland
+        </p>
+        <p>+353 86 196 0451</p>
+        <p>hello@evercam.io</p>
+        <p><i class="fa fa-twitter"></i> <a href="https://twitter.com/evrcm" target="_blank">@evrcm</a></p>
+      </div>
+      <div class="contact-form">
+        <form accept-charset="UTF-8" action="https://formkeep.com/f/3295d2de6a4c" method="POST">
+          <input type="hidden" name="utf8" value="✓">
+          <input type="text" name="name" placeholder="Your Name">
+          <input type="email" name="email" placeholder="Your Email">
+          <textarea type="textarea" rows="4" name="message" placeholder="Your message"></textarea>
+          <button class="btn btn-grey-outline" type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
   </section>
- <? include 'footer.php'; ?>
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" async></script>
- <script src="/js/bootstrap.min.js" async></script>
- <script src="/js/custom.min.js" async></script>
- <script src="../js/contact-google-map.js"></script>
+  <? include 'footer.php'; ?>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" async></script>
+  <script src="/js/bootstrap.min.js" async></script>
+  <script src="/js/custom.min.js" async></script>
+  <script src="../js/contact-google-map.js"></script>
 </body>
 </html>
