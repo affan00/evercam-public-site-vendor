46e4261204884305f4a66173dd7f2b6d78ccf1b7
Maciej Goszczycki <mgoszcz2@gmail.com>
2015-07-03 16:39:42 +0100

    Fix tag mismatch and spacing errors
    Remove old style declartions

--- team.php
+++ team.php
@@ -28,142 +28,142 @@
     <div class="alt-color">
       <section id="team">
         <div class="team">
-          <div class="team-individual">
-            <img alt="Vinnie Quinn" src="http://1.gravatar.com/avatar/5fc9d9bb491dc7604e966937c51c9d66?s=230" style="border-radius:50%;">
-            <h2>Vinnie Quinn
-              <a data-toggle="tooltip" href="https://twitter.com/vinniequinn" target="_blank" title="twitter">
-                <span class="fa fa-twitter"></span>
-              </a>
-              <a data-toggle="tooltip" href="http://ie.linkedin.com/in/vinniequinn" target="_blank" title="linkedin">
-                <span class="fa fa-linkedin"></span>
-              </a>
-            </h2>
-            <h4>Hustler <small>- Former Christmas tree salesman.</small>
-            </h4>
-          </div>
-
-          <div class="team-individual">
-            <img alt="Marco Herbst" src="http://1.gravatar.com/avatar/b4fd03e586651eef82a903fa3b2718bd?s=230" style="border-radius:50%;">
-            <h2>Marco Herbst
-              <a data-toggle="tooltip" href="https://twitter.com/marcoherbst" target="_blank" title="twitter">
-                <span class="fa fa-twitter"></span>
-              </a>
-              <a data-toggle="tooltip" href="http://www.linkedin.com/in/marcoherbst" target="_blank" title="linkedin">
-                <span class="fa fa-linkedin"></span>
-              </a>
-              <a data-toggle="tooltip" href="https://github.com/marcoherbst" target="_blank" title="github">
-                <span class="fa fa-github"></span>
-              </a>
-            </h2>
-            <h4>Hustler <small>- Failed piano player</small>
-            </h4>
-          </div>
-
-          <div class="team-individual">
-            <img alt="Liuting Du" src="/img/liuting.jpg">
-            <h2>Liuting Du
-              <a data-toggle="tooltip" href="https://twitter.com/liutingdu" target="_blank" title="twitter">
-                <span class="fa fa-twitter"></span>
-              </a>
-              <a data-toggle="tooltip" href="https://ie.linkedin.com/in/liutingdu" target="_blank" title="linkedin">
-                <span class="fa fa-linkedin"></span>
-              </a>
-              <a data-toggle="tooltip" href="https://github.com/liutingdu" target="_blank" title="github">
-                <span class="fa fa-github"></span>
-              </a>
-            </h2>
-            <h4>Hacker <small>- Beijing's candidate for Rose of Tralee.</small>
-            </h4>
-          </div>
-
-          <div class="team-individual">
-            <img alt="Vinnie O'Farrell" src="https://secure.gravatar.com/avatar/f264f6540152ad362bbcb3e60d413765?s=230" style="border-radius:50%;">
-            <h2>Vinnie O'Farrell
-              <small>
-                <a data-toggle="tooltip" href="https://ie.linkedin.com/in/vinnieofarrell" target="_blank" title="linkedin">
-                  <span class="fa fa-linkedin"></span>
-                </a>
-              </small>
-            </h2>
-            <h4>Engineer <small>- Johnny Five meets Cable Guy.</small></h4>
-          </div>
-
-          <div class="team-individual">
-            <img alt="Miloš Mošić" src="/img/milos.png">
-            <h2>Miloš Mošić
-              <small>
-                <a data-toggle="tooltip" href="https://github.com/mosic" target="_blank" title="github">
-                  <span class="fa fa-github"></span>
-                </a>
-              </small>
-            </h2>
-            <h4>Developer <small>- Keeping afloat while working remote</small></h4>
-          </div>
-
-          <div class="team-individual">
-            <img alt="Maciej" src="https://gravatar.com/avatar/64ce0ac7d44d2076d23b21ac62214970?s=230" style="border-radius:50%;">
-            <h2>Maciej Goszczycki
-              <small>
-                <a data-toggle="tooltip" href="http://mgoszcz2.com/" target="_blank" title="website">
-                  <span class="fa fa-globe"></span>
-                </a>
-                <a data-toggle="tooltip" href="https://twitter.com/mgoszcz2" target="_blank" title="twitter">
+          <div class="row">
+            <div class="team-individual">
+              <img alt="Vinnie Quinn" src="http://1.gravatar.com/avatar/5fc9d9bb491dc7604e966937c51c9d66?s=230">
+              <h2>Vinnie Quinn
+                <a data-toggle="tooltip" href="https://twitter.com/vinniequinn" target="_blank" title="twitter">
                   <span class="fa fa-twitter"></span>
                 </a>
-                <a data-toggle="tooltip" href="https://github.com/mgoszcz2" target="_blank" title="github">
-                  <span class="fa fa-github"></span>
-                </a>
-              </small>
-            </h2>
-            <h4>Hacker <small>- Hardest thing I had to write all week</h4>
-            </h4>
-          </div>
-
-          <div class="team-individual">
-            <img alt="Azhar" src="https://secure.gravatar.com/avatar/e0b1154b3f230e4451535b2e35c615e7?s=230" style="border-radius:50%;">
-            <h2>Azhar Malik
-              <small>
-                <a data-toggle="tooltip" href="https://pk.linkedin.com/pub/azhar-malik/43/975/530" target="_blank" title="linkedin">
+                <a data-toggle="tooltip" href="http://ie.linkedin.com/in/vinniequinn" target="_blank" title="linkedin">
                   <span class="fa fa-linkedin"></span>
                 </a>
-                <a data-toggle="tooltip" href="https://github.com/azharmalik3" target="_blank" title="github">
-                  <span class="fa fa-github"></span>
-                </a>
-              </small>
-            </h2>
-            <h4>Hacker <small>- Hardest working programmer in Pakistan.</small>
-            </h4>
-          </div>
+              </h2>
+              <h4>Hustler <small>- Former Christmas tree salesman.</small></h4>
+            </div>
 
-          <div class="team-individual">
-            <img alt="Shakeel Anjum" src="https://secure.gravatar.com/avatar/bc5816bd84db9de699f207423d55db18?s=230" style="border-radius:50%;">
-            <h2>Shakeel Anjum
-              <small>
-                <a data-toggle="tooltip" href="https://pk.linkedin.com/pub/m-shakeel-anjum/52/858/59" target="_blank" title="linkedin">
+            <div class="team-individual">
+              <img alt="Marco Herbst" src="http://1.gravatar.com/avatar/b4fd03e586651eef82a903fa3b2718bd?s=230">
+              <h2>Marco Herbst
+                <a data-toggle="tooltip" href="https://twitter.com/marcoherbst" target="_blank" title="twitter">
+                  <span class="fa fa-twitter"></span>
+                </a>
+                <a data-toggle="tooltip" href="http://www.linkedin.com/in/marcoherbst" target="_blank" title="linkedin">
                   <span class="fa fa-linkedin"></span>
                 </a>
-                <a data-toggle="tooltip" href="https://github.com/mshakeelanjum" target="_blank" title="github">
+                <a data-toggle="tooltip" href="https://github.com/marcoherbst" target="_blank" title="github">
                   <span class="fa fa-github"></span>
                 </a>
-              </small>
-            </h2>
-            <h4>Hacker <small>- Malaria survivor and gentle soul.</small></h4>
+              </h2>
+              <h4>Hustler <small>- Failed piano player</small></h4>
+            </div>
+
+            <div class="team-individual">
+              <img alt="Liuting Du" src="/img/liuting.jpg">
+              <h2>Liuting Du
+                <a data-toggle="tooltip" href="https://twitter.com/liutingdu" target="_blank" title="twitter">
+                  <span class="fa fa-twitter"></span>
+                </a>
+                <a data-toggle="tooltip" href="https://ie.linkedin.com/in/liutingdu" target="_blank" title="linkedin">
+                  <span class="fa fa-linkedin"></span>
+                </a>
+                <a data-toggle="tooltip" href="https://github.com/liutingdu" target="_blank" title="github">
+                  <span class="fa fa-github"></span>
+                </a>
+              </h2>
+              <h4>Hacker <small>- Beijing's candidate for Rose of Tralee.</small></h4>
+            </div>
+          </div>
+          <div class="row">
+            <div class="team-individual">
+              <img alt="Vinnie O'Farrell" src="https://secure.gravatar.com/avatar/f264f6540152ad362bbcb3e60d413765?s=230">
+              <h2>Vinnie O'Farrell
+                <small>
+                  <a data-toggle="tooltip" href="https://ie.linkedin.com/in/vinnieofarrell" target="_blank" title="linkedin">
+                    <span class="fa fa-linkedin"></span>
+                  </a>
+                </small>
+              </h2>
+              <h4>Engineer <small>- Johnny Five meets Cable Guy.</small></h4>
+            </div>
+
+            <div class="team-individual">
+              <img alt="Miloš Mošić" src="/img/milos.png">
+              <h2>Miloš Mošić
+                <small>
+                  <a data-toggle="tooltip" href="https://github.com/mosic" target="_blank" title="github">
+                    <span class="fa fa-github"></span>
+                  </a>
+                </small>
+              </h2>
+              <h4>Developer <small>- Keeping afloat while working remote</small></h4>
+            </div>
+
+            <div class="team-individual">
+              <img alt="Maciej" src="https://gravatar.com/avatar/64ce0ac7d44d2076d23b21ac62214970?s=230">
+              <h2>Maciej Goszczycki
+                <small>
+                  <a data-toggle="tooltip" href="http://mgoszcz2.com/" target="_blank" title="website">
+                    <span class="fa fa-globe"></span>
+                  </a>
+                  <a data-toggle="tooltip" href="https://twitter.com/mgoszcz2" target="_blank" title="twitter">
+                    <span class="fa fa-twitter"></span>
+                  </a>
+                  <a data-toggle="tooltip" href="https://github.com/mgoszcz2" target="_blank" title="github">
+                    <span class="fa fa-github"></span>
+                  </a>
+                </small>
+              </h2>
+              <h4>Hacker <small>- Hardest thing I had to write all week</small></h4>
+            </div>
           </div>
 
-          <div class="team-individual">
-            <img alt="Daniel Caffrey" src="https://media.licdn.com/mpr/mpr/shrink_200_200/p/3/000/1d3/255/2c3c94b.jpg">
-            <h2>Daniel Caffrey
-              <a data-toggle="tooltip" href="https://twitter.com/scandox" target="_blank" title="twitter">
-                <span class="fa fa-twitter"></span>
-              </a>
-              <a data-toggle="tooltip" href="https://ie.linkedin.com/in/caffreydaniel" target="_blank" title="linkedin">
-                <span class="fa fa-linkedin"></span>
-              </a>
-              <a data-toggle="tooltip" href="https://github.com/scandox" target="_blank" title="github">
-                <span class="fa fa-github"></span>
-              </a>
-            </h2>
-            <h4>Hacker <small>- Admires Jordan Mechner</small></h4>
+          <div class="row">
+            <div class="team-individual">
+              <img alt="Azhar" src="https://secure.gravatar.com/avatar/e0b1154b3f230e4451535b2e35c615e7?s=230">
+              <h2>Azhar Malik
+                <small>
+                  <a data-toggle="tooltip" href="https://pk.linkedin.com/pub/azhar-malik/43/975/530" target="_blank" title="linkedin">
+                    <span class="fa fa-linkedin"></span>
+                  </a>
+                  <a data-toggle="tooltip" href="https://github.com/azharmalik3" target="_blank" title="github">
+                    <span class="fa fa-github"></span>
+                  </a>
+                </small>
+              </h2>
+              <h4>Hacker <small>- Hardest working programmer in Pakistan.</small></h4>
+            </div>
+
+            <div class="team-individual">
+              <img alt="Shakeel Anjum" src="https://secure.gravatar.com/avatar/bc5816bd84db9de699f207423d55db18?s=230">
+              <h2>Shakeel Anjum
+                <small>
+                  <a data-toggle="tooltip" href="https://pk.linkedin.com/pub/m-shakeel-anjum/52/858/59" target="_blank" title="linkedin">
+                    <span class="fa fa-linkedin"></span>
+                  </a>
+                  <a data-toggle="tooltip" href="https://github.com/mshakeelanjum" target="_blank" title="github">
+                    <span class="fa fa-github"></span>
+                  </a>
+                </small>
+              </h2>
+              <h4>Hacker <small>- Malaria survivor and gentle soul.</small></h4>
+            </div>
+
+            <div class="team-individual">
+              <img alt="Daniel Caffrey" src="https://media.licdn.com/mpr/mpr/shrink_200_200/p/3/000/1d3/255/2c3c94b.jpg">
+              <h2>Daniel Caffrey
+                <a data-toggle="tooltip" href="https://twitter.com/scandox" target="_blank" title="twitter">
+                  <span class="fa fa-twitter"></span>
+                </a>
+                <a data-toggle="tooltip" href="https://ie.linkedin.com/in/caffreydaniel" target="_blank" title="linkedin">
+                  <span class="fa fa-linkedin"></span>
+                </a>
+                <a data-toggle="tooltip" href="https://github.com/scandox" target="_blank" title="github">
+                  <span class="fa fa-github"></span>
+                </a>
+              </h2>
+              <h4>Hacker <small>- Admires Jordan Mechner</small></h4>
+            </div>
           </div>
         </div>
       </section>
